1d90bca71187e96a82dfc3a33e9bea4bcfae0746
Julien Faure <julien.faure@iaca-electronique.com>
2024-09-05 15:24:23 +0200

    Refactor olimex-a20.sh and remove dependency to binary

--- scripts/targets/olimex-a20.sh
+++ scripts/targets/olimex-a20.sh
@@ -4,19 +4,17 @@
 #
 # Requirements :
 #   - losetup
+#   - dd
 #
-# Error : From 40 to 49
+# Error : From 100 to 109
 #   - 100 : Bad usage or image not found
-#   - 101 : U-boot file not found
-#   - 102 : Loop issue
-#   - 103 : U-boot write issue
+#   - 101 : Boot retrieving data from source issue
+#   - 102 : Boot writing issue
 # ============================================
 
 # ---------------------------------------------- OVERRIDABLE VARIABLES
 TARGET_FIRST_SECTOR=20480
 # ----------------------------------------------
-
-UBOOT_RAW_DATA=./targets/assets/u-boot-sunxi-with-spl.bin
 
 # ----------------------------------------------
 
@@ -25,33 +23,27 @@ if [ -z "$UTILS_SOURCED" ]; then
   source ./utils.sh || { echo "ERROR : utils.sh not found."; exit 23; }
 fi
 
+# ----------------------------------------------
+
 function _check_permissions() {
-
     [ ! -f "$INPUT_IMAGE" ] && { error "Input image '$INPUT_IMAGE' not found"; exit 100; }
-    [ ! -f "$UBOOT_RAW_DATA" ] && { error "U-boot file '$UBOOT_RAW_DATA' not found"; exit 101; }
-
+    [ ! -f "$OUTPUT_IMAGE" ] && { error "Output image '$OUTPUT_IMAGE' not found"; exit 100; }
 }
 
+# ----------------------------------------------
 
 function apply_target_configuration(){
   INPUT_IMAGE=$1
+  OUTPUT_IMAGE=$2
 
   _check_permissions
 
-  LOOP_DEVICE=$(losetup -fP --show "$IMAGE_FILE")
+  echo "Copying BOOT to new image.."
+  dd if="$INPUT_IMAGE" of="$OUTPUT_IMAGE" bs=1k skip=8 seek=8 count=1024 conv=notrunc > /dev/null
   if [ $? -ne 0 ]; then
-    error "Unable to load image in loop."
-    exit 102
-  fi
-
-  echo "Writing U-BOOT binary.."
-  dd if="$UBOOT_RAW_DATA" of="$LOOP_DEVICE" bs=1k seek=8 conv=notrunc > /dev/null
-  if [ $? -ne 0 ]; then
-    error "U-boot write issue."
-    exit 103
+    error "Boot copy issue."
+    exit 101
   fi
 
   info "Image configured for Olimex A20 board."
-
-  losetup -d "$LOOP_DEVICE"
 }
